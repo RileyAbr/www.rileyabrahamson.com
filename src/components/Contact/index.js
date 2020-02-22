46007242a12599f2c112593a9fdd54df23519381
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-02-22 10:38:57 -0600

    Began creating contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,40 +6,45 @@ function Contact() {
     return (
         <div className="page-content">
             <form id="contact-form">
-                <div class='contact-row'>
-                    <label for='name'>Name</label>
-                    <input id='input-name' name='name'
-                        type='text' placeholder="Your Name" />
-                </div>
 
-                <div class='contact-row'>
-                    <label for='company'>Company</label>
-                    <input id='input-company' name='company' type='text' placeholder="Your Company" />
-                </div>
+                <h2 id="contact-title">Need Something More Direct?</h2>
 
-                <div class='contact-row'>
-                    <label for='email'>Email</label>
-                    <input id='input-email' name='email' type='email' placeholder="Your Email" />
-                </div>
+                <fieldset class="contact-body">
+                    <div class='contact-row'>
+                        <label for='name'>Name</label>
+                        <input id='input-name'
+                            name='name'
+                            type='text'
+                            placeholder="Your Name" />
+                    </div>
 
-                <div class='contact-row'>
-                    <label for='reason'>Reason for Contact</label>
-                    <select id='input-reason' name='reason'>
-                        <option value='employment'>Employment Inquiry</option>
-                        <option value='collab'>Project Collaboration</option>
-                        <option value='freelance'>Freelance Work</option>
-                        <option value='bug'>Bug Report</option>
-                        <option value='chat'>Just Chattin'</option>
-                    </select>
-                </div>
+                    <div class='contact-row'>
+                        <label for='email'>Email</label>
+                        <input id='input-email'
+                            name='email'
+                            type='email'
+                            placeholder="name@website.com" />
+                    </div>
 
-                <div class='contact-row'>
-                    <label for='message'>Field for other message details</label>
-                    <textarea id='input-message' name='message' placeholder="Any other Details?"></textarea>
-                </div>
+                    <div class='contact-row'>
+                        <label for='reason'>Reason for Contact</label>
+                        <select id='input-reason' name='reason'>
+                            <option value='employment'>Employment Inquiry</option>
+                            <option value='collab'>Project Collaboration</option>
+                            <option value='freelance'>Freelance Work</option>
+                            <option value='bug'>Bug Report</option>
+                            <option value='chat'>Just Chattin'</option>
+                        </select>
+                    </div>
 
-                <div class='contact-row btn-row'>
-                    <button type="submit">Send it!</button>
+                    <div class='contact-row'>
+                        <label for='message'>Field for other message details</label>
+                        <textarea id='input-message' name='message' placeholder="Any else?"></textarea>
+                    </div>
+                </fieldset>
+
+                <div class='contact-submit-row'>
+                    <button class="contact-submit-button" type="submit">Send it!</button>
                 </div>
             </form>
         </div>
