06d15225b167121dcde3b06bd9ac0af8e2dcb424
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-02-23 01:08:42 -0600

    Cleaned up responsiveness on portfolio modals

--- src/components/Portfolio/PortfolioProject/PortfolioModal/index.js
+++ src/components/Portfolio/PortfolioProject/PortfolioModal/index.js
@@ -17,26 +17,36 @@ export default class PortfolioModal extends React.Component {
             return null;
         }
         return (
-            <div class="portfolio-modal">
-                <div class="portfolio-modal-header">
-                    <h4 class="portfolio-modal-title" id="modal-basic-title">Test</h4>
-                    <button type="button" class="modal-close close" aria-label="Close">
-                        <span aria-hidden="true" onClick={e => {
-                            this.onClose(e);
-                        }}>&times;</span>
-                    </button>
-                </div>
-                <div class="portfolio-modal-body">
-                    <p class="modal-desc">{this.props.children}</p>
-                    <img class="portfolio-modal-preview-image" src={""} alt=""></img>
-                    {/* <video class="portfolio-modal-preview-clip" src={""} controls muted></video> */}
-                    <p class="portfolio-modal-tech">Technologies used: Test</p>
-                </div>
-                <div class="portfolio-modal-footer">
-                    <a class="portfolio-modal-btn btn-outline-dark" href={""}
-                        target="_blank" rel="noopener noreferrer">Link to Project</a>
-                    <a class="portfolio-modal-btn" href={""}
-                        target="_blank" rel="noopener noreferrer">Link to GitHub Repo</a>
+            <div>
+                <div className="portfolio-modal-backdrop"
+                ></div>
+
+                <div className="portfolio-modal-wrapper"
+                    onClick={e => {
+                        this.onClose(e);
+                    }}>
+                    <div class="portfolio-modal">
+                        <div class="portfolio-modal-header">
+                            <h4 class="portfolio-modal-title" id="modal-basic-title">Test</h4>
+                            <button type="button" class="modal-close close" aria-label="Close">
+                                <span aria-hidden="true" onClick={e => {
+                                    this.onClose(e);
+                                }}>&times;</span>
+                            </button>
+                        </div>
+                        <div class="portfolio-modal-body">
+                            <p class="modal-desc">{this.props.children}</p>
+                            <img class="portfolio-modal-preview-image" src={""} alt=""></img>
+                            {/* <video class="portfolio-modal-preview-clip" src={""} controls muted></video> */}
+                            <p class="portfolio-modal-tech">Technologies used: Test</p>
+                        </div>
+                        <div class="portfolio-modal-footer">
+                            <a class="portfolio-modal-btn btn-outline-dark" href={""}
+                                target="_blank" rel="noopener noreferrer">Link to Project</a>
+                            <a class="portfolio-modal-btn" href={""}
+                                target="_blank" rel="noopener noreferrer">Link to GitHub Repo</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
