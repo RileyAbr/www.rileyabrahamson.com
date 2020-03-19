cba8e80a1f3d5b0f3f662e07441c0a0e0636b1ce
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-03-19 13:30:33 -0500

    Styled about bio page
    The about page still needs content though

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import './styles.scss';
+
 function About() {
     return (
-        <div>
-            This is the About Page!
+        <div className="page-content">
+            <section className="about-column">
+                <h1 className="about-title">Hello There!</h1>
+                <p className="about-bio">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, consectetur temporibus? Accusamus quod omnis minima quos veniam possimus ipsum soluta. Nesciunt, consequuntur non! Excepturi sunt doloribus laborum? Cum, corrupti delectus?
+                </p>
+            </section>
         </div>
     );
 }
