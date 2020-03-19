a048e6f3975affc8d94e35e5888ca6f2e66aaad2
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-03-19 13:33:17 -0500

    Added bare layout to Resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import "./styles.scss";
+
 function Resume() {
     return (
-        <div>
-            This is the Resume Page!
+        <div className="page-content">
+            <section className="resume-column">
+                Resume Page!
+            </section>
         </div>
     );
 }
