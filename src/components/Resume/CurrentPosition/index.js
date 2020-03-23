de160364f39d8712010a6752dab061825a35e6bf
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-03-23 15:00:27 -0500

    Began creating styles for position boxes in Resume

---
+++ src/components/Resume/CurrentPosition/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import "./styles.scss";
+
+function CurrentPosition(props) {
+    return (
+        <div className="resume-position">
+            <h1 className="resume-position-title">{props.title}</h1>
+            <div className="resume-postion-hr"></div>
+            <a
+                href={props.imgLink}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img
+                    className="resume-position-image"
+                    src={props.imgSrc}
+                    alt={props.altText}
+                />
+            </a>
+        </div>
+    );
+}
+
+export default CurrentPosition;
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import SkillsGraph from './SkillsGraph';
 
+import CurrentPosition from './CurrentPosition';
+
 import "./styles.scss";
 
 function Resume() {
     return (
         <div className="page-content">
             <section className="resume-column">
-                Resume Page!
-                <SkillsGraph></SkillsGraph>
+                {/* JSON Resume Download */}
+
+                {/* Positions Info */}
+                <div className="resume-positions-container">
+                    <CurrentPosition
+                        title={"I'm Currently Studying At"}
+                        imgSrc={"https://via.placeholder.com/450x300"}
+                        altText={"Test"}
+                        imgLink={"https://www.rileyabrahamson.com"}
+                    />
+                    <CurrentPosition
+                        title={"I'm Currently Studying At"}
+                        imgSrc={"https://via.placeholder.com/450x300"}
+                        altText={"Test"}
+                        imgLink={"https://www.rileyabrahamson.com"}
+                    />
+                    <CurrentPosition
+                        title={"I'm Currently Studying At"}
+                        imgSrc={"https://via.placeholder.com/450x300"}
+                        altText={"Test"}
+                        imgLink={"https://www.rileyabrahamson.com"}
+                    />
+                    {/* <div className="resume-position">
+                        I'm Currently Studying At
+                        <img className="resume-position-image" src="https://via.placeholder.com/450x300" />
+                    </div>
+                    <div className="resume-position">
+                        I'm Currently Working At
+                    </div>
+                    <div className="resume-position">
+                        I'm Currently President Of
+                    </div>
+                    <div className="resume-position">
+                        Other Places I've Worked
+                    </div> */}
+                </div>
+
+                {/* Skills Graph */}
+                <div className="resume-skills-graph">
+                    <SkillsGraph></SkillsGraph>
+                </div>
             </section>
         </div>
     );
