cae113c815dc03f3596706adb1d8c0a371a55815
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-03-23 14:04:26 -0500

    Added a responsive container to the skills chart

--- src/components/Resume/SkillsGraph/index.js
+++ src/components/Resume/SkillsGraph/index.js
@@ -5,7 +5,8 @@ import {
     XAxis,
     YAxis,
     CartesianGrid,
-    Tooltip
+    Tooltip,
+    ResponsiveContainer
 } from 'recharts';
 
 import scssVariables from '../../../scss/_variables.scss';
@@ -18,7 +19,7 @@ const data = [
         name: 'CSS', Years: 3,
     },
     {
-        name: 'JS', Years: 2,
+        name: 'JavaScript', Years: 2,
     },
     {
         name: 'Python', Years: 2,
@@ -27,32 +28,33 @@ const data = [
 ];
 
 export default class Example extends PureComponent {
-    static jsfiddleUrl = 'https://jsfiddle.net/alidingling/30763kr7/';
 
     render() {
         return (
-            <BarChart
-                layout="vertical"
-                width={500}
-                height={300}
-                data={data}
-                margin={{
-                    top: 5, right: 30, left: 20, bottom: 20,
-                }}
-            >
-                <CartesianGrid horizontal={false} strokeDasharray="5 5" />
-                <XAxis allowDecimals={false}
-                    type="number"
-                    stroke={scssVariables.cborderprimary}
-                    label={{ value: 'Years of Experience', position: 'bottom', offset: 0 }} />
-                <YAxis type="category" dataKey="name" stroke={scssVariables.cborderprimary} />
-                <Tooltip />
-                <Bar dataKey="Years"
-                    stroke={scssVariables.cborderpurple}
-                    strokeWidth={2}
-                    fill={scssVariables.cbackgroundpurple}
-                    fillOpacity={1} />
-            </BarChart>
+            <ResponsiveContainer width="100%" height={300}>
+                <BarChart
+                    layout="vertical"
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5, right: 30, left: 20, bottom: 20,
+                    }}
+                >
+                    <CartesianGrid horizontal={false} strokeDasharray="5 5" />
+                    <XAxis allowDecimals={false}
+                        type="number"
+                        stroke={scssVariables.cborderprimary}
+                        label={{ value: 'Years of Experience', position: 'bottom', offset: 0 }} />
+                    <YAxis type="category" dataKey="name" stroke={scssVariables.cborderprimary} />
+                    <Tooltip />
+                    <Bar dataKey="Years"
+                        stroke={scssVariables.cborderpurple}
+                        strokeWidth={2}
+                        fill={scssVariables.cbackgroundpurple}
+                        fillOpacity={1} />
+                </BarChart>
+            </ResponsiveContainer>
         );
     }
 }
