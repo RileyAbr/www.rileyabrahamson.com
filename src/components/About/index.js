7b58837c052b5cb9e03ab797ef76f235a8712bc2
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-07-05 17:05:41 -0500

    Wrote up a bio for the About page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-import './styles.scss';
+import "./styles.scss";
 
 function About() {
     return (
@@ -8,7 +8,25 @@ function About() {
             <section className="about-column">
                 <h1 className="about-title">Hello There!</h1>
                 <p className="about-bio">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, consectetur temporibus? Accusamus quod omnis minima quos veniam possimus ipsum soluta. Nesciunt, consequuntur non! Excepturi sunt doloribus laborum? Cum, corrupti delectus?
+                    I'm Riley Abrahamson, a web developer and current student at
+                    North Dakota State University. I'm studying a combination of
+                    computer science, web design, and strategic
+                    communcations--as well as a handful of German! I aspire to
+                    work in the games industry in the future and spend my free
+                    time running NDSU's League of Legends Club as an officer for
+                    the third year in a row. I'm passionate about being an
+                    inclusivity in games and software development; I'm always
+                    eager to learn anything ally or{" "}
+                    <a id="about-a11y" href="https://a11yproject.com/">
+                        A11Y
+                    </a>
+                    .
+                </p>
+                <br />
+                <p className="about-bio">
+                    Lastly, I'm a huge ice cream snob and won't hesitate to let
+                    you know my opinion on your sweet, frozen treat of choice.
+                    🍨
                 </p>
             </section>
         </div>
