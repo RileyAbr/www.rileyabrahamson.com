0f9df958e4aa1ff508b0eb0ecaffd92a77a49478
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-08-31 21:33:35 -0500

    Fixed ESlint errors

--- src/components/Home/HomeSocialButton/index.js
+++ src/components/Home/HomeSocialButton/index.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './styles.scss';
+import "./styles.scss";
 
 import HomeButton from "../HomeButton";
 
 class HomeSocialButton extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <HomeButton
@@ -16,15 +12,20 @@ class HomeSocialButton extends Component {
                 parentCallback={this.props.parentCallback}
                 faceDirect={this.props.faceDirect}
             >
-                <a className="home-nav-button"
+                <a
+                    className="home-nav-button"
                     href={this.props.externalLink}
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <img className="home-nav-icon" src={this.props.imgSrc} alt={this.props.altText}></img>
+                    <img
+                        className="home-nav-icon"
+                        src={this.props.imgSrc}
+                        alt={this.props.altText}
+                    ></img>
                 </a>
             </HomeButton>
-        )
+        );
     }
 }
 
